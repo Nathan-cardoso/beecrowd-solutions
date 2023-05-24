eaff14ce466bc349a5af6a23b5b63204c2fe9804
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-24 14:38:40 -0300

    add: new quest

---
+++ python/quest-1011.py
@@ -0,0 +1,7 @@
+#Resolução da questão 1011
+#Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). [...]
+
+PI = 3.14159
+r = float(input())
+
+print('VOLUME = {:.3f}'.format(float(4/3)*PI*(r**3)))
